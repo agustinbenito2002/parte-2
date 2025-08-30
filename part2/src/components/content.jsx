025c44d6b0374eac3fb3d52fd601909473377acd
Agustin Benito <agusbenito2002@gmail.com>
2025-08-30 16:08:04 -0300

    ej 2.4

--- part2/src/components/content.jsx
+++ part2/src/components/content.jsx
@@ -1,24 +1,19 @@
 import Part from './Part';
 
 const Content = ({ parts }) => {
-  // Usamos reduce para sumar todos los ejercicios
   const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
 
   return (
     <div>
-      {/* Mostramos cada parte */}
       {parts.map((part) => (
         <Part key={part.id} name={part.name} exercises={part.exercises} />
       ))}
 
-      {/* Mostramos la suma al final */}
       <p>
-        <strong>Total of exercises: {totalExercises}</strong>
+        <strong>total of {totalExercises} exercises</strong>
       </p>
     </div>
   );
 };
 
 export default Content;
-
-
--- part2/src/components/header.jsx
+++ part2/src/components/header.jsx
@@ -1,5 +1,6 @@
 const Header = ({ name }) => {
-  return <h1>{name}</h1>;
+  return <h2>{name}</h2>;
 };
 
 export default Header;
+
