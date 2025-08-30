03961b66afe67dcafcf429f6caf7307adada6689
Agustin Benito <agusbenito2002@gmail.com>
2025-08-30 15:59:09 -0300

    git commit

--- part2/src/components/content.jsx
+++ part2/src/components/content.jsx
@@ -1,14 +1,23 @@
 import Part from './Part';
 
 const Content = ({ parts }) => {
+  // Calculamos el total de ejercicios con reduce
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+
   return (
     <div>
       {parts.map((part) => (
         <Part key={part.id} name={part.name} exercises={part.exercises} />
       ))}
+
+      {/* Mostramos la suma al final */}
+      <p>
+        <strong>Total of exercises: {totalExercises}</strong>
+      </p>
     </div>
   );
 };
 
 export default Content;
+
 
