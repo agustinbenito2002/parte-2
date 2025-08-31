0674f8e95c66a0c8cb5184d363f3288dfc84a0f8
Agustin Benito <agusbenito2002@gmail.com>
2025-08-31 14:15:54 -0300

    2.11

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,37 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
 
-  // Maneja cambios en el input de nombre
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+  // Cargar datos iniciales desde db.json
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
-  // Maneja cambios en el input de número
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setFilter(event.target.value)
 
-  // Maneja cambios en el campo de búsqueda
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value)
-  }
-
-  // Maneja la adición de una nueva persona
   const addPerson = (event) => {
     event.preventDefault()
 
-    // Verificamos si el nombre ya existe
     const nameExists = persons.some(
       person => person.name.toLowerCase() === newName.toLowerCase()
     )
@@ -41,17 +35,21 @@ const App = () => {
       return
     }
 
-    const personObject = {
+    const newPerson = {
       name: newName,
       number: newNumber
     }
 
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
+    // Enviar la nueva persona al servidor
+    axios
+      .post('http://localhost:3001/persons', newPerson)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
   }
 
-  // Filtramos las personas según el término de búsqueda
   const personsToShow = persons.filter(person =>
     person.name.toLowerCase().includes(filter.toLowerCase())
   )
@@ -60,12 +58,10 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
 
-      {/* Filtro de búsqueda */}
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
 
       <h3>Add a new</h3>
 
-      {/* Formulario para agregar personas */}
       <PersonForm
         addPerson={addPerson}
         newName={newName}
@@ -76,7 +72,6 @@ const App = () => {
 
       <h3>Numbers</h3>
 
-      {/* Lista de personas */}
       <Persons persons={personsToShow} />
     </div>
   )
